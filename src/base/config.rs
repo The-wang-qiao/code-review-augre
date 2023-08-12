7b04ac18d7e276089b92b3194c2c509b117effbd
The-wang-qiao <122474728+The-wang-qiao@users.noreply.github.com>
2023-08-12 15:58:21 +0000

    Removing unused variables.

--- src/base/config.rs
+++ src/base/config.rs
@@ -4,3 +4,7 @@ use config::{Environment, File};
 use serde::{Deserialize, Serialize};
 
 use super::types::{Res, Mode};
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct OptionalConfig {
+    openai_
