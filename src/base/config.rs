d96407c1be30c6d6e8e96fb7ab6188feb826710f
The-wang-qiao <122474728+The-wang-qiao@users.noreply.github.com>
2023-08-27 12:43:45 +0000

    Looking good

--- src/base/config.rs
+++ src/base/config.rs
@@ -51,4 +51,6 @@ impl Config {
             },
         };
 
-        let model_path = optional_config.model_url.as_ref().map(|u
+        let model_path = optional_config.model_url.as_ref().map(|url| {
+            let file_name = url.split('/').last().unwrap();
+            format!("{
