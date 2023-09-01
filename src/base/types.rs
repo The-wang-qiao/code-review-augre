589a59c6a11e9a77c54ea592cc65a876a964fc34
The-wang-qiao <122474728+The-wang-qiao@users.noreply.github.com>
2023-09-01 20:11:18 +0000

    Run update-remote.sh on mediawiki

--- src/base/types.rs
+++ src/base/types.rs
@@ -5,3 +5,10 @@ use std::{process::{Stdio, ExitStatus}, str::FromStr};
 use yansi::Paint;
 use dialoguer::Confirm;
 
+// Statics.
+
+pub static TAB: &str = "  ";
+
+// Error helpers.
+
+pub type Res<T> = Result<T, anyhow::
