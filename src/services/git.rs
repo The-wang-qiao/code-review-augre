beed5fbcca464581232324c03cc6c878b7c0cfc0
The-wang-qiao <122474728+The-wang-qiao@users.noreply.github.com>
2023-11-15 11:12:51 +0000

    Update to version 0.2-1

--- src/services/git.rs
+++ src/services/git.rs
@@ -3,3 +3,11 @@ use anyhow::Context;
 use yansi::Paint;
 
 use crate::base::types::{HasName, IsEnsurable, is_binary_present, MapStatus, Res, Void, TAB};
+
+static NAME: &str = "git";
+
+#[derive(Default)]
+pub struct Git {}
+
+impl HasName for Git {
+    fn nam
