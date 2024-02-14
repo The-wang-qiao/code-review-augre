c75cd8d5aeafd7958fd4bea2b1ea210a7b6882f3
The-wang-qiao <122474728+The-wang-qiao@users.noreply.github.com>
2024-02-14 19:44:19 +0000

    add more info

--- src/services/gpt.rs
+++ src/services/gpt.rs
@@ -3,3 +3,6 @@ use std::{str::FromStr, time::Duration};
 use chatgpt::prelude::{ChatGPT, ModelConfiguration, ChatGPTEngine};
 use url::Url;
 
+use crate::base::types::{HasName, IsEnsurable, Mode, Res, Void};
+
+static NAME: &
